724eaaed50d298f6df1ef72756ae881f3e88c624
Ilysikov <i_lysikov@bk.ru>
2024-03-19 22:17:50 +0300

    Dictionary0

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+
+import sys
+import pathlib
+
+BASE_DIR = pathlib.Path(__file__).parent.parent.parent.parent
+project = BASE_DIR.name
+
+# Добавляем проект с модулями в путь Python
+sys.path.insert(0, str(BASE_DIR /'Users/ivanlysikov/PycharmProjects/pythonProject6'))
+# extensions = [,
+# ]
+templates_path = ["_templates"]
+exclude_patterns = []
+language = "ru"
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
